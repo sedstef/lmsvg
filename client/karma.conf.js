09f84e40dd2b3afe8ad2a6423407da3a73a3c86c
Stefan Sedelmaier <stefan@sedelmaier.at>
2020-05-19 08:19:47 +0200

    add basic test, add karma reports for sonarqube and jenkins junit

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -9,18 +9,38 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
+      require('karma-junit-reporter'),
+      require('karma-sonarqube-unit-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
+    //report in JUnit format for Jenkins
+    junitReporter: {
+      outputDir: require('path').join(__dirname, 'build/karma-reports'),
+      suite: 'src/app',
+      useBrowserName: true, // add browser name outputFile
+      xmlVersion: null,//use junit format
+      nameFormatter: function (browser, result) {
+        return result.fullName;
+      },
+      classNameFormatter: function (browser, result) {
+        return result.suite;
+      }
+    },
+    sonarQubeUnitReporter: {
+      outputFile: require('path').join(__dirname, 'build/ut_report.xml'),
+      suite: 'src/app',
+      useBrowserName: true
+    },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage'),
+      dir: require('path').join(__dirname, 'build/coverage'),
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'junit', 'sonarqubeUnit'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
